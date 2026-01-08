1d077d4b75b5ed07286a5c14f154a9dc1a5f3ee4
G KRISHNA <smlkittu@gmail.com>
2026-01-08 21:57:03 +0530

    Add files via upload

---
+++ Day2 py features,datatypes,variables,naming rules.py.py
@@ -0,0 +1,18 @@
+name="krishna"
+age=20
+bank_balance=1606.99
+is_student=True
+Friends=[5,6,7,8,3,5,9]
+list1=["apple","banana","grapes","mango"]
+date=(7,"jan",2026)
+roll_number={101,102,103,104}
+user_name={"login":"krishna123","password":"krishna@123"}
+print("My name is:",name)
+print("my age is:",age)
+print("my bank balance is:",bank_balance)
+print("Am i a student:",is_student)
+print("Number of friends i have:",Friends)
+print("my favourite fruits are:",list1)
+print("Today's date is:",date)
+print("my roll numbers are:",roll_number)
+print("my login credentials are:",user_name)
