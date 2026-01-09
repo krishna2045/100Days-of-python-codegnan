59902a0afa10573c4b3efa2ae3b72b71b1e4fbf7
G KRISHNA <smlkittu@gmail.com>
2026-01-09 08:20:09 +0530

    Add files via upload

---
+++ Day-4 Topic-2 Operators.py
@@ -0,0 +1,51 @@
+#OPERATORS:
+#Arithmetic operator:+,-,*,/,%,//,**
+#Simple-Calculator program:
+# a=10
+# b=20
+# addition=a+b
+# subraction=b-a
+# multiplication=a*b
+# division=b/a
+# floordivision=b//a
+# modules=b%a
+# exponent=b**a
+# print("sum of two numbers:",addition)
+# print("sub of two numbers:",subraction)
+# print("multiple of two numbers:",multiplication)
+# print("divisible of two numbers:",division)
+# print("floordivision of two numbers:",floordivision)
+# print("modules of two numbers:",modules)
+# print("exponent:",exponent)
+
+# 2.Comparison/Relational Operators:==,!=,>,<,>=,<=
+a=10
+b=18
+print(a==b)  #False
+print(a!=b)  #True
+print(a>b)   #False
+print(a<b)   #True
+print(a>=b)  #False
+print(a<=b)  #True
+
+# 3.Logical Operators: and,or,not
+
+#AND:
+a=False
+b=True
+print(a and a)
+print(a and b)
+print(b and a)
+print(b and b)
+
+#OR:
+print(a or a)
+print(a or b)
+print(b or a)
+print(b or b)
+
+#NOT:
+print(not a)
+print(not b)
+
+
