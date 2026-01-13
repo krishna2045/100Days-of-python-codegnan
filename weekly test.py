7dcf143e0af3c804b8495c63ceac4c0b4260dbff
G KRISHNA <smlkittu@gmail.com>
2026-01-14 05:11:37 +0530

    Add files via upload
    Weekly Test | Python 100 Days Challenge 🐍📝
    
    Completed my weekly assessment focused on Control & Conditional Statements in Python.
    
    🔹 Exam Topics Covered:
    
    ✔️ if statements
    ✔️ if–else conditions
    ✔️ if–elif–else decision-making
    ✔️ Logical and comparison operators within conditions
    
    The test helped me evaluate my understanding of decision-making logic and how Python executes different conditions based on input. These concepts are crucial for building real-world applications and problem-solving skills.
    
    Learning, practicing, and testing knowledge — one step closer to mastering Python! 🚀💻
    consistency intensity:
    @codeganan
    
    #Python #Python100DaysChallenge #WeeklyTest #IfElse #Elif #CodingJourney #LearnPython

---
+++ weekly test.py
@@ -0,0 +1,68 @@
+#WEEKLY TEST-1
+#1.CHECK EVEN OR ODD :
+'''
+num=int(input("enter a number:"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+'''
+#FIND THE LARGEST OF TWO NUMBER:
+'''
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+if a>b:
+    print(a)
+elif b>a:
+    print(b)
+else:
+    print("equal")
+'''
+'''
+#CHECK ELIGIBILITY TO VOTE:
+age =int(input("Enter a age:"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print("Not Eligibility")
+'''
+#GRADE MARKS:
+'''
+num=int(input("Enter a number:"))
+if num>=90:
+    print("A-Grade")
+elif num>=75:
+    print("B-Grade")
+else:
+    print("Fail")
+'''
+#FIND THREE LRAGEST NUMBER:
+a=int(input("enter a first number:"))
+b=int(input("enter a second number:"))
+c=int(input("enter a third number:"))
+if a>b and a>c:
+    print("A is largest number")
+elif b<b and b>c:
+    print("B is largest number")
+else:
+    print("C is largest number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
