877d30ba720e37befc2a14bbf5f315b1ed97f988
G KRISHNA <smlkittu@gmail.com>
2026-01-22 20:05:54 +0530

    Add files via upload

---
+++ Day-7.1-While-loop.py
@@ -0,0 +1,25 @@
+# while loop
+'''
+num=1 #variable initialization
+while num<11: #condition
+    print(num)
+    num+=1 #increment/decrement
+'''
+# print even numbers from 1 to 20
+
+'''
+num=1
+while num<=20:
+    if num%2==0:
+        print(num)
+    num+=1
+'''
+# print odd numbers from 1 to 20
+
+'''
+num=1
+while num<=20:
+    if num%2!=0:
+        print(num)
+    num+=1
+'''
