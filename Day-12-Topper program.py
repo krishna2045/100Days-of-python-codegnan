61076412ac8cb75932c12ce86f067b04362f105b
G KRISHNA <smlkittu@gmail.com>
2026-01-28 22:04:31 +0530

    Add files via upload

---
+++ Day-12-Topper program.py
@@ -0,0 +1,51 @@
+#who is topper of the class
+print("welcome to the topper")
+n=int(input("enter number of the student in the class:"))
+names= []
+marks=[]
+class_marks=[]
+percentage=[]
+
+print("Enter the all student marks")
+for i in range(n):
+    print(f"Enter the roll number{i+1}student marks")
+    name=input("Enter the student name:")
+    print("Enter the Telugu,English,Hindi,Maths,Science,Social marks,as per the oder")
+    Telugu,English,Hindi,Maths,Science,Social=map(int,input().split()[:6])
+    total_marks=Telugu+English+Hindi+Maths+Science+Social
+    std_percentage=(total_marks/600)*100
+# store all values in list
+    names.append(name)
+    marks.append([Telugu,English,Hindi,Maths,Science,Social])
+    class_marks.append(total_marks)
+    percentage.append(std_percentage)
+#print the topper details
+print("Topper in the class is")
+top_marks=max(class_marks)
+topper_pos=[]
+for i in range(n):
+    if class_marks[i]==top_marks:
+        topper_pos.append(i)
+#print the topper details
+for i in range(len(topper_pos)):
+    print("Name of the student is:",names[i])
+    print("Marks of the student is:",marks[i])
+#Each student marks
+for i in range(n):
+    print("Name of the student is:",names[i],"Marks is:",marks[i])
+    
+# #Find the smallest value in the list
+# lst=[1,2,4,5,6,-5]
+# smallest=lst[0]
+# for i in range(1,len(lst)):
+#     if lst[i]<smallest:
+#         smallest=lst[i]
+# print("Smallest value in the list is:",smallest)
+
+#Find the largest value in the list
+# lst=[1,2,4,5,6,-5]
+# largest=lst[0]
+# for i in range(1,len(lst)):
+#     if lst[i]>largest:
+#         largest=lst[i]
+# print("Largest value in the list is:",largest)
